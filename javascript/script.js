10171bdfc2a81c828a1f66d1c3c05c3d6de56e6a
Colin <colingoodale@gmail.com>
2018-08-15 21:39:46 -0500

    finished product

--- javascript/script.js
+++ javascript/script.js
@@ -1,26 +1,48 @@
 var buttonArray = ['dog', 'cat', ' goldfish'];
+
+//create global event listener
 function createButton() {
+    $("#buttons").empty();
     for (i = 0; i < buttonArray.length; i++) {
-        var button = $(`<button>${buttonArray[i]}</button>`);
-        button.val(`${buttonArray[i]}`);
-        button.data(`${buttonArray[i]}`);
-        button.addClass('searchBtn');
+        var button = $(`<button>`);
+        button.addClass("buttons");
+        button.attr("dataName", buttonArray[i]);
+        button.text(buttonArray[i]);
         $("#buttons").append(button);
     }
 }
-createButton();
-$(".searchBtn").on('click', function () {
-    var input = $(this).val();
+$(".buttons").on('click', displayGiph());
+
+function displayGiph() {
+    var input = $(this).attr("data-name");
     console.log(input)
-    var queryURL = `https://api.giphy.com/v1/gifs/search?q=${input}&api_key=d4RfkXHMO0Rn1L5OuDEeBvnP57G8yt9x`;
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + input + "&api_key=d4RfkXHMO0Rn1L5OuDEeBvnP57G8yt9x&1";
 
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function (response) {
-        console.log(response);
+        //append response to container div
+        for (let i = 0; i < response.data.length; i++) {
+            a = $("<img>");
+            a.attr("src", response.data[i].images.downsized.url)
+            a.text(response.data[i].title)
+            $("#container").append(a);
+        }
+        console.log(response.data[i].images.downsized.url);
     });
-})
-$("#submit").on("click", function () {
+}
+$("#submit").on("click", function (event) {
+    event.preventDefault();
+    // push value into array
+    gifRequest = $("#gifSearch").val().trim();
+    buttonArray.push(gifRequest)
 
-})
+    //call create button
+    createButton();
+
+});
+
+$(document).on("click", ".buttons", displayGiph);
+
+createButton();
